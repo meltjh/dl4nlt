6752ef84bd3428520304a010eec0a64ebe250bd4
tirzaelise <tirza.soute@student.uva.nl>
2018-09-27 13:58:03 +0200

    higher hidden size

--- lstm/train_lstm.py
+++ lstm/train_lstm.py
@@ -22,7 +22,7 @@ EPOCHS = 20
 NUM_CLASSES = 2
 EMBEDDING_DIM = 50
 SEQUENCE_LENGTH = 500
-NUM_HIDDEN = 32 # geprobeerd 32, 128
+NUM_HIDDEN = 256 # geprobeerd 32, 128
 NUM_LAYERS = 1
 
 def get_accuracy(predictions, targets):
@@ -36,13 +36,13 @@ def train():
     # For colab code update
     importlib.reload(get_data)
     from get_data import get_dataset
-    
-    # dataloader_train, _, _ = get_datasets(BATCH_SIZE)
+
     dataloader_train = get_dataset("train", BATCH_SIZE)
     num_batches = len(dataloader_train)
 
     # Initialize the model, optimizer and loss function
-    model = LSTM(EMBEDDING_DIM, BATCH_SIZE, NUM_CLASSES, NUM_HIDDEN, NUM_LAYERS).to(device)
+    model = LSTM(EMBEDDING_DIM, BATCH_SIZE, NUM_CLASSES, NUM_HIDDEN,
+                 NUM_LAYERS).to(device)
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
     loss_function = nn.CrossEntropyLoss()
 
@@ -61,15 +61,15 @@ def train():
             outputs = model(x)
             single_loss = loss_function(outputs, y)
             single_loss.backward()
-            
+
             sum_loss += float(single_loss)
-            
+
             # torch.nn.utils.clip_grad_norm_(model.parameters(), 0.25) # Nog niet zeker wat dit doet, maar iig tegen vanishing / exploding gradient, ps, werkt niet :'(
-            
+
             optimizer.step()
 
             sum_accuracy += get_accuracy(outputs, y)
-            
+
         accuracy = sum_accuracy / num_batches
         loss = sum_loss / num_batches
 
